ae97c5539ab6242cb0c0ff5af7ae214b0a6211bc
Priyadharshan Reddy SEELAM <priyadharshanreddy.seelam@amadeus.com>
2021-02-08 21:08:15 +0530

    added Dates, Main classes

--- src/helpers/dates.py
+++ src/helpers/dates.py
@@ -4,81 +4,75 @@ import datetime
 from collections import OrderedDict
 
 
-def findall_dates(file_contents):
-    date_pattern = r"\n[\d]{1,2}/[\d]{1,2}/[\d]{2}"
-    dates_list = re.findall(date_pattern, file_contents)
-    # to remove \n from date, using date[1:]
-    dates_list_formatted = [date[1:] for date in dates_list]
-    # OrderedDict.fromkeys removes duplicates while preserving order
-    return list(OrderedDict.fromkeys(dates_list_formatted))
+class Dates:
+    def __init__(self):
+        self.dates_list = []
+        self.date_time_list = []
+        self.total_days = 0
+        self.message_days = 0
+        self.longest_convo_messages = 0
+        self.longest_convo_start = None
+        self.longest_convo_end = None
+        self.messages_per_hour = []
+        self.messages_per_day = []
 
+    def findall_dates(self, file_contents):
+        date_pattern = r"\n[\d]{1,2}/[\d]{1,2}/[\d]{2}"
+        self.dates_list = re.findall(date_pattern, file_contents)
+        # to remove \n from date, using date[1:]
+        self.dates_list = [date[1:] for date in self.dates_list]
+        # OrderedDict.fromkeys removes duplicates while preserving order
+        self.dates_list = list(OrderedDict.fromkeys(self.dates_list))
 
-def findall_date_time(file_contents):
-    date_time_pattern = r"\n[\d]{1,2}/[\d]{1,2}/[\d]{2}, [\d]{2}:[\d]{2}"
-    date_time_list = re.findall(date_time_pattern, file_contents)
-    date_time_list_formatted = [date_time[1:] for date_time in date_time_list]
-    return date_time_list_formatted, list(OrderedDict.fromkeys(date_time_list_formatted))
+    def findall_date_time(self, file_contents):
+        date_time_pattern = r"\n[\d]{1,2}/[\d]{1,2}/[\d]{2}, [\d]{2}:[\d]{2}"
+        self.date_time_list = re.findall(date_time_pattern, file_contents)
+        self.date_time_list = [parse(date_time[1:]) for date_time in self.date_time_list]
 
+    def count_days(self, dates_list):
+        start_date = parse(dates_list[0])
+        end_date = parse(dates_list[-1])
+        self.total_days = (end_date - start_date).days
+        self.message_days = len(dates_list)
 
-def count_days(dates_list):
-    start_date = parse(dates_list[0])
-    end_date = parse(dates_list[-1])
-    total_days = (end_date - start_date).days
-    message_days = len(dates_list)
-    return total_days - message_days, total_days
-
-
-def parse_date_time_list(date_time_list):
-    parsed_list = [parse(date_time) for date_time in date_time_list]
-    return parsed_list
-
-
-def longest_conversation(date_time_list):
-    messages_number = 0
-    i = -1
-    j = 0
-    while j < len(date_time_list):
-        i += 1
-        j = i+1
-        counter = 0
-        if (date_time_list[j] - date_time_list[i]).seconds < 600:
-            temp = i
-        else:
-            continue
-        while j < len(date_time_list) and (date_time_list[j] - date_time_list[i]).seconds < 600:
-            counter += 1
+    def longest_conversation(self, date_time_list):
+        i = -1
+        j = 0
+        while j < len(date_time_list):
             i += 1
-            j += 1
-        if counter > messages_number:
-            messages_number = counter
-            end_time_index = j-1
-            start_time_index = temp
-    return messages_number, start_time_index, end_time_index
+            j = i+1
+            counter = 0
+            if (date_time_list[j] - date_time_list[i]).seconds < 600:
+                temp = i
+            else:
+                continue
+            while j < len(date_time_list) and (date_time_list[j] - date_time_list[i]).seconds < 600:
+                counter += 1
+                i += 1
+                j += 1
+            if counter > self.longest_convo_messages:
+                self.longest_convo_messages = counter
+                self.longest_convo_end = j-1
+                self.longest_convo_start = temp
 
+    def find_peak_hour(self, date_time_list):
+        messages_in_hour = {}
+        for date_time in date_time_list:
+            if date_time.hour not in messages_in_hour:
+                messages_in_hour[date_time.hour] = 1
+            else:
+                messages_in_hour[date_time.hour] += 1
+        self.messages_per_hour = sorted(messages_in_hour.items(), key=lambda item: item[1], reverse=True)
+        self.messages_per_hour = [(datetime.time(hour[0]), hour[1]) for hour in self.messages_per_hour]
 
-def find_peak_hour(date_time_list):
-    messages_in_hour = {}
-    for date_time in date_time_list:
-        if date_time.hour not in messages_in_hour:
-            messages_in_hour[date_time.hour] = 1
-        else:
-            messages_in_hour[date_time.hour] += 1
-    messages_in_hour_list = sorted(messages_in_hour.items(), key=lambda item: item[1], reverse=True)
-    # print(messages_in_hour_list)
-    peak_hour = datetime.time(messages_in_hour_list[0][0])
-    return peak_hour, messages_in_hour_list[0][1]
-
-
-def find_peak_day(date_time_list):
-    messages_in_day = {}
-    day_mapping = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
-    for date_time in date_time_list:
-        if date_time.weekday() not in messages_in_day:
-            messages_in_day[date_time.weekday()] = 1
-        else:
-            messages_in_day[date_time.weekday()] += 1
-    messages_in_day_list = sorted(messages_in_day.items(), key=lambda item: item[1], reverse=True)
-    # print(messages_in_day_list)
-    peak_day = day_mapping[messages_in_day_list[0][0]]
-    return peak_day, messages_in_day_list[0][1]
+    def find_peak_day(self, date_time_list):
+        messages_in_day = {}
+        day_mapping = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+        for date_time in date_time_list:
+            if date_time.weekday() not in messages_in_day:
+                messages_in_day[date_time.weekday()] = 1
+            else:
+                messages_in_day[date_time.weekday()] += 1
+        self.messages_per_day = sorted(messages_in_day.items(), key=lambda item: item[1], reverse=True)
+        self.messages_per_day = [(day_mapping[day[0]], day[1]) for day in self.messages_per_day]
 
--- src/main.py
+++ src/main.py
@@ -1,98 +1,114 @@
 import traceback
 import math
 
-from helpers import dates, names, generic
+from helpers import names, generic
 from friend import Friend
+from helpers.dates import Dates
 
 
-def main():
-    try:
-        file_path = "../sample3.txt"
-        file_contents = generic.read_file(file_path)
-        #print(file_contents)
-        friend1 = Friend()
-        friend2 = Friend()
+class Main:
+    def __init__(self):
+        self.file_path = None
+        self.file_contents = None
 
-        friend1.name, friend2.name = names.find_names(file_contents)
-        print("Friend1 is {}".format(friend1.name))
-        print("Friend2 is {}".format(friend2.name))
+    def main(self):
+        try:
+            self.file_path = "../sample.txt"
+            self.file_contents = generic.read_file(self.file_path)
+            #print(file_contents)
+            friend1 = Friend()
+            friend2 = Friend()
+            date_object = Dates()
 
-        friend1.parsed_name, friend2.parsed_name = names.names_parsed(friend1.name,friend2.name)
+            friend1.name, friend2.name = names.find_names(self.file_contents)
+            print("Friend1 is {}".format(friend1.name))
+            print("Friend2 is {}".format(friend2.name))
 
-        friend1.find_message_count(file_contents)
-        friend2.find_message_count(file_contents)
-        print("Number of messages sent by {} is {}".format(friend1.name, friend1.message_count))
-        print("Number of messages sent by {} is {}".format(friend2.name, friend2.message_count))
+            friend1.parsed_name, friend2.parsed_name = names.names_parsed(friend1.name,friend2.name)
 
-        friend1.prepare_messages_list(file_contents)
-        friend2.prepare_messages_list(file_contents)
+            friend1.find_message_count(self.file_contents)
+            friend2.find_message_count(self.file_contents)
+            print("Number of messages sent by {} is {}".format(friend1.name, friend1.message_count))
+            print("Number of messages sent by {} is {}".format(friend2.name, friend2.message_count))
 
-        friend1.find_word_frequency()
-        friend2.find_word_frequency()
-        print("Number of media sent by {} is {}".format(friend1.name, friend1.media_shared))
-        print("Number of media sent by {} is {}".format(friend2.name, friend2.media_shared))
-        print("Number of words sent by {} is {}".format(friend1.name, friend1.word_count))
-        print("Number of words sent by {} is {}".format(friend2.name, friend2.word_count))
+            friend1.prepare_messages_list(self.file_contents)
+            friend2.prepare_messages_list(self.file_contents)
 
-        friend1.find_frequently_used_words()
-        friend2.find_frequently_used_words()
-        print("We are considering words with 3 or more letters for frequently used words, words are not case sensitive")
-        print("Frequently used words sent by {} are {}".format(friend1.name, friend1.frequently_used_words))
-        print("Frequently used words sent by {} are {}".format(friend2.name, friend2.frequently_used_words))
+            friend1.find_word_frequency()
+            friend2.find_word_frequency()
+            print("Number of media sent by {} is {}".format(friend1.name, friend1.media_shared))
+            print("Number of media sent by {} is {}".format(friend2.name, friend2.media_shared))
+            print("Number of words sent by {} is {}".format(friend1.name, friend1.word_count))
+            print("Number of words sent by {} is {}".format(friend2.name, friend2.word_count))
 
-        dates_list = dates.findall_dates(file_contents)
-        date_time_list, date_time_list_unique = dates.findall_date_time(file_contents)
-        date_time_list = dates.parse_date_time_list(date_time_list)
-        messages_count, start_time_index, end_time_index = dates.longest_conversation(date_time_list)
-        no_message_days, total_days = dates.count_days(dates_list)
-        friend1.messages_per_day = math.ceil(friend1.message_count/total_days)
-        friend2.messages_per_day = math.ceil(friend2.message_count/total_days)
+            friend1.find_frequently_used_words()
+            friend2.find_frequently_used_words()
+            print("We are considering words with 3 or more letters for frequently used words, words are not case "
+                  "sensitive")
+            print("Frequently used words sent by {} are {}".format(friend1.name, friend1.frequently_used_words))
+            print("Frequently used words sent by {} are {}".format(friend2.name, friend2.frequently_used_words))
 
-        print("Number of days without any message is {} from a total of {}".format(no_message_days, total_days))
-        print("Messages per day by {} is {}".format(friend1.name, friend1.messages_per_day))
-        print("Messages per day by {} is {}".format(friend2.name, friend2.messages_per_day))
-        print("Total length of all messages put together by {} is {}".format(friend1.name,
-                                                                             friend1.total_messages_length))
-        print("Total length of all messages put together by {} is {}".format(friend2.name,
-                                                                             friend2.total_messages_length))
-        print("Longest Message by {} is {} \nLength is {}".format(friend1.name, friend1.longest_message,
-                                                                  friend1.longest_message_length))
-        print("Longest Message by {} is {} \nLength is {}".format(friend2.name, friend2.longest_message,
-                                                                  friend2.longest_message_length))
-        print("For finding the longest conversation, we have considered a maximum time gap of 10 minutes between "
-              "adjacent messages")
-        print("Longest conversation has {} messages, started at {}, ended at {}".format(messages_count,
-                                                                                        date_time_list[start_time_index],
-                                                                                        date_time_list[end_time_index]))
-        print("Average length of each message by {} is {}".format(friend1.name, friend1.average_message_length))
-        print("Average length of each message by {} is {}".format(friend2.name, friend2.average_message_length))
-        print("Average number of words per message by {} is {}".format(friend1.name, friend1.average_words_per_message))
-        print("Average number of words per message by {} is {}".format(friend2.name, friend2.average_words_per_message))
+            date_object.findall_dates(self.file_contents)
+            date_object.findall_date_time(self.file_contents)
+            date_object.longest_conversation(date_object.date_time_list)
+            date_object.count_days(date_object.dates_list)
+            friend1.messages_per_day = math.ceil(friend1.message_count/date_object.total_days)
+            friend2.messages_per_day = math.ceil(friend2.message_count/date_object.total_days)
 
-        friend1.find_frequently_used_emojis()
-        friend2.find_frequently_used_emojis()
-        # print("Frequently used emojis by {} are {}".format(friend1.name, friend1.frequently_used_emojis))
-        # print("Frequently used emojis by {} are {}".format(friend2.name, friend2.frequently_used_emojis))
-        print("Frequently used emojis by {} with description {}".format(friend1.name, friend1.emojis_with_description))
-        print("Frequently used emojis by {} with description {}".format(friend2.name, friend2.emojis_with_description))
+            print("Number of days without any message is {} from a total of {}".format(
+                date_object.total_days - date_object.message_days, date_object.total_days))
+            print("Messages per day by {} is {}".format(friend1.name, friend1.messages_per_day))
+            print("Messages per day by {} is {}".format(friend2.name, friend2.messages_per_day))
+            print("Total length of all messages put together by {} is {}".format(friend1.name,
+                                                                                 friend1.total_messages_length))
+            print("Total length of all messages put together by {} is {}".format(friend2.name,
+                                                                                 friend2.total_messages_length))
+            print("Longest Message by {} is {} \nLength is {}".format(friend1.name, friend1.longest_message,
+                                                                      friend1.longest_message_length))
+            print("Longest Message by {} is {} \nLength is {}".format(friend2.name, friend2.longest_message,
+                                                                      friend2.longest_message_length))
+            print("For finding the longest conversation, we have considered a maximum time gap of 10 minutes between "
+                  "adjacent messages")
+            print("Longest conversation has {} messages, started at {}, ended at {}".
+                  format(date_object.longest_convo_messages,
+                         date_object.date_time_list[date_object.longest_convo_start],
+                         date_object.date_time_list[date_object.longest_convo_end]))
+            print("Average length of each message by {} is {}".format(friend1.name, friend1.average_message_length))
+            print("Average length of each message by {} is {}".format(friend2.name, friend2.average_message_length))
+            print("Average number of words per message by {} is {}".format(friend1.name,
+                                                                           friend1.average_words_per_message))
+            print("Average number of words per message by {} is {}".format(friend2.name,
+                                                                           friend2.average_words_per_message))
 
-        messages_at_peak_hour = dates.find_peak_hour(date_time_list)
-        percent_of_total_messages_hour = round((messages_at_peak_hour[1]/(friend1.message_count + friend2.message_count))*100, 2)
-        print("Most number of messages were sent at during {} hour of the day. "
-              "Total messages at this hour are {} which is {}% of total messages".
-              format(messages_at_peak_hour[0], messages_at_peak_hour[1], percent_of_total_messages_hour))
+            friend1.find_frequently_used_emojis()
+            friend2.find_frequently_used_emojis()
+            # print("Frequently used emojis by {} are {}".format(friend1.name, friend1.frequently_used_emojis))
+            # print("Frequently used emojis by {} are {}".format(friend2.name, friend2.frequently_used_emojis))
+            print("Frequently used emojis by {} with description {}".format(friend1.name,
+                                                                            friend1.emojis_with_description))
+            print("Frequently used emojis by {} with description {}".format(friend2.name,
+                                                                            friend2.emojis_with_description))
 
-        messages_at_peak_day = dates.find_peak_day(date_time_list)
-        percent_of_total_messages_day = round(
-            (messages_at_peak_day[1] / (friend1.message_count + friend2.message_count)) * 100, 2)
-        print("Most number of messages were sent on {}. "
-              "Total messages on this day are {} which is {}% of total messages".format(messages_at_peak_day[0],
-                                                                                        messages_at_peak_day[1],
-                                                                                        percent_of_total_messages_day))
+            date_object.find_peak_hour(date_object.date_time_list)
+            percent_of_total_messages_hour = round(
+                (date_object.messages_per_hour[0][1]/(friend1.message_count + friend2.message_count))*100, 2)
+            print("Most number of messages were sent at during {} hour of the day. "
+                  "Total messages at this hour are {} which is {}% of total messages".
+                  format(date_object.messages_per_hour[0][0], date_object.messages_per_hour[0][1],
+                         percent_of_total_messages_hour))
 
-    except Exception:
-        print(traceback.format_exc())
+            date_object.find_peak_day(date_object.date_time_list)
+            percent_of_total_messages_day = round(
+                (date_object.messages_per_day[0][1] / (friend1.message_count + friend2.message_count)) * 100, 2)
+            print("Most number of messages were sent on {}. "
+                  "Total messages on this day are {} which is {}% of total messages".
+                  format(date_object.messages_per_day[0][0], date_object.messages_per_day[0][1],
+                         percent_of_total_messages_day))
+
+        except Exception:
+            print(traceback.format_exc())
 
 
 if __name__ == "__main__":
-    main()
+    main_obj = Main()
+    main_obj.main()
