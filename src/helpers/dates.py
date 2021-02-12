36b082ba7d6e9039489fa4a1842538f43c7613f0
Priyadharshan Reddy SEELAM <priyadharshanreddy.seelam@amadeus.com>
2021-02-12 22:03:15 +0530

    removed date log

--- src/helpers/dates.py
+++ src/helpers/dates.py
@@ -38,9 +38,7 @@ class Dates:
     def count_days(self, dates_list):
         logging.debug("count_days::enter")
         start_date = parse(dates_list[0])
-        logging.debug("start date %s", start_date)
         end_date = parse(dates_list[-1])
-        logging.debug("end date %s", end_date)
         self.total_days = (end_date - start_date).days + 1
         self.message_days = len(dates_list)
         self.no_message_days = self.total_days - self.message_days
